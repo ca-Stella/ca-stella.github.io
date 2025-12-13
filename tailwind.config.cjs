8570697733156417f341759de163fc3d8cfbe322
ca-Stella <stella.2532@gmail.com>
2025-12-12 20:13:08 -0700

    Set up tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+// tailwind.config.cjs
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx,js,jsx}",
+    "./src/*.{ts,tsx,js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
