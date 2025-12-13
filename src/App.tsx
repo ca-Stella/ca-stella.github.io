f30abe4c984e168d44345641018168fc7e581d8e
ca-Stella <stella.2532@gmail.com>
2025-12-12 20:17:21 -0700

    Clean demo code

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './App.css'
 
 export default function App() {
   return (
-    <div className="min-h-screen min-w-screen bg-red-500 flex items-center justify-center">
-      <h1 className="text-white text-5xl font-bold">
+    <div className="min-h-screen min-w-screen bg-gradient-to-b from-amber-50 to-orange-50 flex items-center justify-center">
+      <h1 className="bg-gradient-to-r from-amber-600 via-orange-500 to-yellow-600 bg-clip-text text-transparent">
         Hello world!
       </h1>
     </div>
