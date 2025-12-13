b18ceea29a9491d8efb6dd48e47fba0886286562
ca-Stella <stella.2532@gmail.com>
2025-12-13 14:40:01 -0700

    Add simple hero section with name

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Hero from "./components/Hero";
+
 
 export default function App() {
   return (
     <div className="min-h-screen min-w-screen bg-gradient-to-b from-amber-50 to-orange-50 flex items-center justify-center">
-      <h1 className="bg-gradient-to-r from-amber-600 via-orange-500 to-yellow-600 bg-clip-text text-transparent">
-        Hello world!
-      </h1>
+      
+    <Hero />
     </div>
   )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+      <h1 className="bg-gradient-to-r from-amber-600 via-orange-400 to-yellow-500 bg-clip-text text-transparent">
+        Stella Heo
+      </h1>
+  )
+}
+
+      
