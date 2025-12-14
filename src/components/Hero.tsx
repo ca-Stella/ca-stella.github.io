799a14e34e63b52f7400d7fe7c0003d019446b56
ca-Stella <stella.2532@gmail.com>
2025-12-13 18:30:46 -0700

    Update hero section with some information

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,17 @@
 export default function Hero() {
   return (
-      <h1 className="bg-gradient-to-r from-amber-600 via-orange-400 to-yellow-500 bg-clip-text text-transparent">
-        Stella Heo
+    <div className="flex flex-col">
+      <h2 className="-mb-3">Hi, I'm</h2>
+      <h1 className="-mb-1 text-center bg-gradient-to-r from-amber-600 via-orange-400 to-yellow-500 bg-clip-text text-transparent">
+        Ca-Stella
       </h1>
+      <h2 className="text-center">Computer Science & Data Science Student</h2>
+      <div className="mt-1">
+        <h3 className="text-center">Baking data into delightful insights</h3>
+        <h3 className="text-center">Making tech as sweet as cake</h3>
+      </div>
+
+      </div>
   )
 }
 
