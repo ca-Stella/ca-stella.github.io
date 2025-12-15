a1f7f4e8a9cc5e4dbda69dbb6e9d257e6816e475
ca-Stella <stella.2532@gmail.com>
2025-12-14 22:52:03 -0700

    Update animation for hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,39 +1,106 @@
 import castella from "./../assets/castella.png";
-import { motion } from "motion/react";
+import { easeOut, motion } from "motion/react";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col  justify-center items-center min-h-screen">
+    <div className="flex flex-col  justify-center items-center min-h-screen overflow-hidden">
       <motion.img
         initial={{ scale: 0, rotate: -180 }}
         animate={{ scale: 1, rotate: 0 }}
         transition={{ duration: 1, delay: 0.2, type: "spring", bounce: 0.4 }}
-        className="mb-8 inline-block relative"
+        className="inline-block relative"
         style={{ maxWidth: "20%", height: "auto" }}
         src={castella}
       />
-      
-      <motion.h1 className="-mt-5 -mb-1 text-center bg-gradient-to-r from-amber-600 via-orange-400 to-yellow-500 bg-clip-text text-transparent"
-              initial={{ opacity: 0, y: 50, rotate: 10 }}
-              animate={{ opacity: 1, y: 0, rotate: 0 }}
-              transition={{
-                duration: 0.5,
-                delay: 0.6,
-                ease: "easeOut",
-              }}> Ca-Stella
-      </motion.h1>
 
-      <h2 className="text-center font-bold text-cinnamon">
+      <h1 className="-mb-4 overflow-hidden">
+        {/* For exact gradient and animation combination, I had to hardcode these ;-; */}
+        <motion.span
+          className="inline-block bg-gradient-to-r from-amber-600 to-[#ea7800] text-transparent bg-clip-text"
+          initial={{ opacity: 0, y: 50, rotate: -10 }} animate={{ opacity: 1, y: 0, rotate: 0 }}
+          transition={{
+            delay: 0.6,
+            duration: 0.4,
+            ease: easeOut,
+          }}> C </motion.span>
+        <motion.span 
+        className="inline-block bg-gradient-to-r from-[#eb7800] to-[#f37e00] text-transparent bg-clip-text"
+          initial={{ opacity: 0, y: 50, rotate: -10 }} animate={{ opacity: 1, y: 0, rotate: 0 }}
+          transition={{
+            delay: 0.65,
+            duration: 0.4,
+            ease: easeOut,
+          }}> a </motion.span>
+        <motion.span className="inline-block bg-gradient-to-r from-[#ec7901] to-[#f88200] text-transparent bg-clip-text"
+          initial={{ opacity: 0, y: 50, rotate: -10 }} animate={{ opacity: 1, y: 0, rotate: 0 }}
+          transition={{
+            delay: 0.7,
+            duration: 0.4,
+            ease: easeOut,
+          }}> - </motion.span>
+        <motion.span className="inline-block bg-gradient-to-r from-[#f98200] to-[#ff8800] text-transparent bg-clip-text"
+          initial={{ opacity: 0, y: 50, rotate: -10 }} animate={{ opacity: 1, y: 0, rotate: 0 }}
+          transition={{
+            delay: 0.75,
+            duration: 0.4,
+            ease: easeOut,
+          }}> S </motion.span>
+        <motion.span className="inline-block bg-gradient-to-r from-[#ff8904] to-[#ff9000] text-transparent bg-clip-text"
+          initial={{ opacity: 0, y: 50, rotate: -10 }} animate={{ opacity: 1, y: 0, rotate: 0 }}
+          transition={{
+            delay: 0.8,
+            duration: 0.4,
+            ease: easeOut,
+          }}> t </motion.span>
+        <motion.span className="inline-block bg-gradient-to-r from-[#ff9200] to-[#fb9b02] text-transparent bg-clip-text"
+          initial={{ opacity: 0, y: 50, rotate: -10 }} animate={{ opacity: 1, y: 0, rotate: 0 }}
+          transition={{
+            delay: 0.85,
+            duration: 0.4,
+            ease: easeOut,
+          }}> e </motion.span>
+        <motion.span className="inline-block bg-gradient-to-r from-[#fb9c01] to-[#f9a100] text-transparent bg-clip-text"
+          initial={{ opacity: 0, y: 50, rotate: -10 }} animate={{ opacity: 1, y: 0, rotate: 0 }}
+          transition={{
+            delay: 0.9,
+            duration: 0.4,
+            ease: easeOut,
+          }}> l </motion.span>
+        <motion.span className="inline-block bg-gradient-to-r from-[#f8a203] to-[#f6a600] text-transparent bg-clip-text"
+          initial={{ opacity: 0, y: 50, rotate: -10 }} animate={{ opacity: 1, y: 0, rotate: 0 }}
+          transition={{
+            delay: 0.95,
+            duration: 0.4,
+            ease: easeOut,
+          }}> l </motion.span>
+        <motion.span className="inline-block bg-gradient-to-r from-[#f5a700] to-yellow-500 text-transparent bg-clip-text"
+          initial={{ opacity: 0, y: 50, rotate: -10 }} animate={{ opacity: 1, y: 0, rotate: 0 }}
+          transition={{
+            delay: 1,
+            duration: 0.4,
+            ease: easeOut,
+          }}> a </motion.span>
+      </h1>
+
+
+      <motion.h2
+        className="text-center font-bold text-cinnamon"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.95, duration: 0.4 }}>
         Computer Science & Data Science Student
-      </h2>
-      <div className="mt-1">
-        <h3 className="text-center text-cinnamon">
+      </motion.h2>
+      <motion.div className="mt-1 text-center text-cinnamon"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        transition={{ delay: 0.95, duration: 0.4 }}>
+        <h3>
           🥣 Baking data into delightful insights
         </h3>
-        <h3 className="text-center text-cinnamon">
+        <h3>
           🍰 Making tech as sweet as cake
         </h3>
-      </div>
+      </motion.div>
     </div>
   );
 }
