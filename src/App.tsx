9a84f96152dbae936c8b365c3f28305aa2d426a3
ca-Stella <stella.2532@gmail.com>
2025-12-15 19:28:54 -0700

    Create Projects component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Hero from "./components/Hero";
 import About from './components/About';
-
+import Projects from './components/Projects';
 export default function App() {
   return (
     <div className="min-h-screen min-w-screen bg-gradient-to-b from-amber-50 to-orange-50 flex flex-col items-center justify-center">
       
     <Hero />
     <About />
+    <Projects />
     </div>
   )
 }
---
+++ src/components/Projects.tsx
@@ -0,0 +1,50 @@
+
+export default function Projects() {
+  // Projects!
+
+  const projects = [
+    {
+      title: "Study Habit Data Visualization Dashboard",
+      description:
+        "Interactive dashboard showcasing study habits of four different students.",
+      tags: ["Tableau"],
+      bgColor: "bg-rose-100",
+
+    },
+    {
+      title: "Goose Park",
+      description:
+        "Online multiplayer board game platform.",
+      tags: ["Java", "JavaFX", "Maven", "JUnit", "TestFX"],
+      bgColor: "bg-purple-100",
+    },
+    {
+      title: "Skylia - Ongoing",
+      description:
+        "Day-to-day symptom tracker, exploring its connection to your environment.",
+      tags: ["Typescript", "Next.js", "TailwindCSS", "Vite"],
+      bgColor: "bg-sky-50",
+    },
+  ];
+
+  return (
+    <section className="min-h-screen flex items-center justify-center flex-col">
+      <h2 className="text-4xl font-semibold mb-8 ">Featured Projects</h2>
+      <div className="max-w-4xl px-6">
+        {projects.map((project) => (
+          // <div className="space-y-16">
+          <div className={`relative ${project.bgColor} p-10`}>
+            <h3 className="text-xl font-semibold">{project.title}</h3>
+
+            <div className="flex gap-3 flex-col">
+              <p className="">{project.description}</p>
+
+              <div className="flex flex-wrap gap-2">{project.tags}</div>
+            </div>
+            {/* </div> */}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
