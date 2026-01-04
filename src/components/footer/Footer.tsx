168248196819821b047d02c8ec0b0990188106bc
ca-Stella <stella.2532@gmail.com>
2026-01-03 20:38:11 -0700

    Update navigation bar and footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,21 +1,25 @@
-import { Heart, Coffee } from "lucide-react";
+import { ChefHat } from "lucide-react";
 
 import { motion } from "motion/react";
 
 export default function Footer() {
-    return (
+  return (
     <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4">
-        <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{opacity : 1}}
-          transition={{ duration: 0.4, delay: 0.2 }}
-          className="text-center pt-4 border-t-4 border-amber-200/50"
-        >
-          <p className="text-amber-700 flex items-center justify-center gap-2 flex-wrap mb-2">
-            <img src="src/assets/castella.png" className="inline h-5"/>© {new Date().getFullYear()} Ca-Stella. All rights reserved.</p>
-        <p className="text-xs text-gray-500 italic">Built with React & Typescript, powered by Vite, styled with Tailwind CSS, and animated using Motion.</p>
-        </motion.div>    
-        </footer>
-  
-    )}
-
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.4, delay: 0.2 }}
+        className="text-center pt-4 border-t-4 border-amber-200/50"
+      >
+        <p className="text-amber-700 flex items-center justify-center gap-1.5 flex-wrap mb-2">
+          <ChefHat size={24} className="-mt-1 inline fill-orange-300 text-amber-800" />
+          © {new Date().getFullYear()} Ca-Stella. All rights reserved.
+        </p>
+        <p className="text-xs text-gray-500 italic">
+          Built with React & Typescript, powered by Vite, styled with Tailwind
+          CSS, and animated using Motion.
+        </p>
+      </motion.div>
+    </footer>
+  );
+}
--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,11 +1,11 @@
-import { ChefHat } from 'lucide-react';
 
 export function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-white/90 backdrop-blur border-b-3 border-yellow-400">
-      <div className="max-w-6xl mx-auto px-6 h-16 flex items-center justify-between">
+
+    <nav className="fixed top-6 left-1/2 -translate-x-1/2 z-50 bg-white/50 backdrop-blur-xl border-b-3 border-yellow-400">
+      <div className="max-w-6xl mx-auto px-6 h-16 gap-20 flex items-center justify-between">
         <div className="flex flex-row  gap-2 justify-center text-4xl  bg-gradient-to-r from-amber-500 via-orange-400 to-yellow-500 bg-clip-text text-transparent">
-          <ChefHat size={32} className=" fill-orange-300 text-amber-800" />
+          
           <h1>Ca-Stella</h1>
         </div>
 
