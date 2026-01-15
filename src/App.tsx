148fe55ba3f7ab271602cc75cd1b779c8e7358e1
ca-Stella <stella.2532@gmail.com>
2026-01-15 15:34:19 -0700

    Prevent scroll restoration on refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import { useEffect } from "react";
+
 import './App.css'
 import Navbar from './components/nav/Navbar';
-
 import Hero from "./components/Hero";
 import About from './components/About';
 import Projects from './components/Projects';
@@ -8,10 +9,21 @@ import Contact from './components/Contact';
 import Footer from './components/footer/Footer';
 import Wave from './layout/Wave';
 
+
 export default function App() {
+    useEffect(() => {
+    // Prevent browser from restoring scroll on refresh
+    if ("scrollRestoration" in window.history) {
+      window.history.scrollRestoration = "manual";
+    }
+
+    // Scroll to top on page load
+    window.scrollTo(0, 0);
+  }, []);
+  
   return (
-    <body className=" overflow-x-hidden">
-    <div className="min-h-screen w-full bg-gradient-to-b from-amber-50 to-orange-50 flex flex-col items-center justify-center grow">
+    // <body className=" overflow--hidden">
+    <div className="min-h-screen w-full bg-gradient-to-b from-amber-50 to-orange-50 flex flex-col items-center justify-center grow overflow-x-hidden">
     <Navbar />
 
     <Hero />
@@ -21,6 +33,7 @@ export default function App() {
     <Contact />
     <Footer />
 
-    </div></body>
+    </div>
+    // </body>
   )
 }
