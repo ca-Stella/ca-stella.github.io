160f2938e0cc58d9687b8b716af477a046ece123
ca-Stella <stella.2532@gmail.com>
2026-01-26 11:58:29 -0700

    Add dashboard sizing/styling

--- src/projects/StudyDashboard.tsx
+++ src/projects/StudyDashboard.tsx
@@ -25,6 +25,58 @@ export default function StudyDashboard() {
     };
   }, []);
 
+  // Tableau original dimensions
+  const originalWidth = 1050;
+  const originalHeight = 1150;
+  const aspectRatio = originalHeight / originalWidth; // 1.1
+
+  // Determine container width and scale based on breakpoints
+  let containerWidth = 0.7 * windowWidth;
+  let scale = 0.8;
+  let heightAdd = 200;
+
+  if (windowWidth < 300) { // 
+    containerWidth = 0.3 * windowWidth;
+    scale = 0.15;
+    heightAdd = 80;
+  } else if (windowWidth < 400) { // 
+    containerWidth = 0.3 * windowWidth;
+    scale = 0.25;
+    heightAdd = 150;
+  } else if (windowWidth < 535) { // sm
+    containerWidth = 0.6 * windowWidth;
+    scale = 0.3;
+    heightAdd = 10;
+  } else if (windowWidth < 590) { // sm
+    containerWidth = 0.6 * windowWidth;
+    scale = 0.4;
+    heightAdd = 45;
+  }  else if (windowWidth < 690) { // md
+    containerWidth = 0.55 * windowWidth;
+    scale = 0.45;
+    heightAdd = 100;
+  }  else if (windowWidth < 800) { // md
+    containerWidth = 0.55 * windowWidth;
+    scale = 0.5;
+    heightAdd = 100;
+  }  else if (windowWidth < 925) { // md
+    containerWidth = 0.55 * windowWidth;
+    scale = 0.6;
+    heightAdd = Math.max(150, containerWidth * 0.1);
+  } else if (windowWidth < 1024) { // md
+    containerWidth = 0.55 * windowWidth;
+    scale = 0.7;
+    heightAdd = 190;
+  }  
+
+  let containerHeight = containerWidth * aspectRatio + heightAdd;
+
+  if (windowWidth >= 1024) {
+    containerWidth = 0.7 * windowWidth;
+    scale = 0.8;
+    containerHeight = 860;
+  }
+
   return (
     <ProjectPage
       title="Study Pattern Dashboard"
